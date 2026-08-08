1966de6b0529dc8ece6f7e3ce7a2743bd90a76ca
Syed Muhammad Nadey Ali <s.nadishah@gmail.com>
2026-08-08 18:57:53 +0500

    Re-enable email verification banner with spam-folder note

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
+import { VerifyEmailBanner } from "./VerifyEmailBanner";
 
 export function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      <VerifyEmailBanner />
       <div className="flex-1 flex flex-col sm:flex-row">
         <Sidebar />
         <main className="flex-1 min-w-0 p-4 pb-24 sm:p-6 sm:pb-6">
